eaa00c7992ca1c2d3c3e4ecd046447efea4e83c7
samarthnayak04 <samarthnayak04@gmail.com>
2025-08-23 00:31:33 +0530

    added Navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <header className="w-full bg-white shadow px-6 py-4 flex justify-end items-center">
+      <button className="bg-purple-600 text-white px-4 py-2 rounded-lg hover:bg-purple-700">
+        Logout
+      </button>
+    </header>
+  );
+}
