1a53070acd3f97c1e0f9cb1c7bf5b27c02c6c112
samarthnayak04 <samarthnayak04@gmail.com>
2025-08-22 21:55:57 +0530

    update config

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,9 @@
+import Dashboard from "./components/Dashboard";
 function App() {
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-blue-500 text-white p-6 rounded-lg shadow-lg text-2xl">
-        Tailwind is working 🎉
-      </div>
-    </div>
+    <>
+      <Dashboard />
+    </>
   );
 }
 
