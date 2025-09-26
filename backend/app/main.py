94aec770185e9701193c342017369b37dcc4ab6b
samarthnayak04 <samarthnayak04@gmail.com>
2025-09-27 00:18:45 +0530

    create main server

---
+++ backend/app/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from app.api.routes import auth,user,fraud
+from app.core.config import settings
+
+app = FastAPI(title=settings.project_name)
+
+# Use the API prefix from settings
+app.include_router(auth.router, prefix=f"{settings.api_v1_str}/auth", tags=["auth"])
+app.include_router(fraud.router, prefix=f"{settings.api_v1_str}/fraud", tags=["fraud"])
+# app.include_router(user.router, prefix=f"{settings.api_v1_str}/user", tags=["user"])
+
