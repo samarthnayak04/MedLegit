c8b3a47c7785c55ef791a2c3786e77ccbfc8ea94
samarthnayak04 <samarthnayak04@gmail.com>
2025-10-04 11:56:59 +0530

    add user relation

--- backend/app/models/health.py
+++ backend/app/models/health.py
@@ -2,6 +2,7 @@ import uuid
 from sqlalchemy import Column, String, Float, ForeignKey, TIMESTAMP, Integer
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy import func
+from sqlalchemy.orm import relationship
 from app.models.user import User
 from app.core.db import Base
 
@@ -13,3 +14,5 @@ class PneumoniaCase(Base):
     prediction = Column(String(50), nullable=False)
     confidence = Column(Float, nullable=False)
     created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
+
+    user = relationship("User", back_populates="pneumonia_cases")
