e1f28fe8206099288dedb5635bcf4f9a5b736366
samarthnayak04 <samarthnayak04@gmail.com>
2025-10-04 11:52:10 +0530

    Improve main application setup and routing

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
 from app.api.routes import auth,user,fraud,health
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 
 app = FastAPI(title=settings.project_name)
+origins = [
+    "http://localhost:5173", 
+     "http://localhost:8000", # Vite
+    
+    # "http://127.0.0.1:5173",
+    
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Use the API prefix from settings
 app.include_router(auth.router, prefix=f"{settings.api_v1_str}/auth", tags=["auth"])
