2ce09853a5e064fa3ef31f571ec912a10706d69b
samarthnayak04 <samarthnayak04@gmail.com>
2025-11-12 21:58:55 +0530

    enhance main.py and dependency loading for faster backend initialization

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,12 +2,19 @@
 from dotenv import load_dotenv
 load_dotenv()
 import warnings
+import os
 warnings.filterwarnings("ignore", message=".*pkg_resources.*")
 from fastapi import FastAPI
-from app.api.routes import auth,user,fraud,health, legal
+from app.api.routes import auth,user,fraud,health, legal,admin
+from transformers import logging as hf_logging
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
 from app.core.db import engine, Base
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+warnings.filterwarnings("ignore", category=FutureWarning)
+hf_logging.set_verbosity_error()
+import tensorflow as tf
+tf.get_logger().setLevel('ERROR')
 
 app = FastAPI(title=settings.project_name)
 origins = [
@@ -32,14 +39,5 @@ app.include_router(fraud.router, prefix=f"{settings.api_v1_str}/fraud", tags=["f
 app.include_router(user.router, prefix=f"{settings.api_v1_str}/user", tags=["user"])
 app.include_router(health.router,prefix=f"{settings.api_v1_str}/health",tags=["health"])
 app.include_router(legal.router,prefix=f"{settings.api_v1_str}/legal",tags=["legal"])
+app.include_router(admin.router,prefix=f"{settings.api_v1_str}/admin",tags=["admin"])
 
-# ✅ Step 4: Create DB tables at startup
-@app.on_event("startup")
-def startup_event():
-    Base.metadata.create_all(bind=engine)
-    print("✅ Database tables created successfully!")
-
-# ✅ Step 5: Root route (optional)
-@app.get("/")
-def read_root():
-    return {"message": "MedLegit API is running"}
