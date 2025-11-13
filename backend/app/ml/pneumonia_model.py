b0c158f3696a71530fdf3cd94e82af884f8e2083
samarthnayak04 <samarthnayak04@gmail.com>
2025-11-13 13:07:25 +0530

    update pneumonia prediction threshold

--- backend/app/ml/pneumonia_model.py
+++ backend/app/ml/pneumonia_model.py
@@ -4,7 +4,7 @@ import io
 import numpy as np
 from PIL import Image
 from tensorflow.keras.models import load_model
-
+from tensorflow.keras.applications.resnet50 import preprocess_input
 class PneumoniaModel:
     def __init__(self, model_path: str):
         # Directly load the full saved model (.keras or .h5)
@@ -14,18 +14,18 @@ class PneumoniaModel:
     def preprocess(self, img_bytes: bytes):
         img = Image.open(io.BytesIO(img_bytes)).convert("RGB")
         img = img.resize((224, 224))
-        arr = np.array(img, dtype=np.float32) / 255.0   # must match training rescale
+        arr = preprocess_input(np.array(img, dtype=np.float32))  # must match training rescale
         arr = np.expand_dims(arr, 0)                   # add batch dimension
         return arr
 
     def predict(self, img_bytes: bytes):
         img_array = self.preprocess(img_bytes)
         pred = self.model.predict(img_array, verbose=0)[0][0]
-        label = "Pneumonia" if pred > 0.5 else "Normal"
-        confidence = float(pred if pred > 0.5 else 1 - pred)
+        label = "Pneumonia" if pred >= 0.7006 else "Normal"
+        confidence = float(pred if pred >= 0.7006 else 1 - pred)
         return label, confidence
 
 
 # ✅ instantiate once for FastAPI
 # Prefer .keras, fallback to .h5
-pneumonia_model = PneumoniaModel("aiModels/disease_predictor/pneumonia_resnet50_classifier.keras")
+pneumonia_model = PneumoniaModel("aiModels/disease_predictor/pneumonia_classifier1.keras")
