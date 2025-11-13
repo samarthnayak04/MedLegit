ba42416e674583c4d8ad644efc53c315514ad6c3
samarthnayak04 <samarthnayak04@gmail.com>
2025-11-13 12:07:19 +0530

    update directory

--- ai_models/legal_agent/config.py
+++
@@ -1,6 +0,0 @@
-# Configuration variables
-import os
-
-UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads")
-OUTPUT_FOLDER = os.path.join(os.getcwd(), "outputs")
-ALLOWED_EXTENSIONS = {"pdf", "docx", "txt"}
